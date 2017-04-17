005b7154dcfe44871b09761e07508e88045f73eb
Amir M. Saeidi <amir.iust@gmail.com>
2017-04-17 18:23:54 +0200

    Add the source packages for the kernel language
    * the conrete and abstract syntax

---
+++ org.servicifi.gelato.language.kernel.resource.kernel/src/org/servicifi/gelato/language/kernel/resource/kernel/analysis/KernelCHARACTER_LITERALTokenResolver.java
@@ -0,0 +1,32 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * 
+ */
+package org.servicifi.gelato.language.kernel.resource.kernel.analysis;
+
+import java.util.Map;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+
+public class KernelCHARACTER_LITERALTokenResolver implements org.servicifi.gelato.language.kernel.resource.kernel.IKernelTokenResolver {
+	
+	private org.servicifi.gelato.language.kernel.resource.kernel.analysis.KernelDefaultTokenResolver defaultTokenResolver = new org.servicifi.gelato.language.kernel.resource.kernel.analysis.KernelDefaultTokenResolver(true);
+	
+	public String deResolve(Object value, EStructuralFeature feature, EObject container) {
+		// By default token de-resolving is delegated to the DefaultTokenResolver.
+		String result = defaultTokenResolver.deResolve(value, feature, container, null, null, null);
+		return result;
+	}
+	
+	public void resolve(String lexem, EStructuralFeature feature, org.servicifi.gelato.language.kernel.resource.kernel.IKernelTokenResolveResult result) {
+		// By default token resolving is delegated to the DefaultTokenResolver.
+		defaultTokenResolver.resolve(lexem, feature, result, null, null, null);
+	}
+	
+	public void setOptions(Map<?,?> options) {
+		defaultTokenResolver.setOptions(options);
+	}
+	
+}
